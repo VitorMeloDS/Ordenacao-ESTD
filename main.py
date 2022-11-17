f7187a590d7369d45680d9b6c07ac036339d0a17
Vitor Melo da Silva <96471272+VitorMeloDS@users.noreply.github.com>
2022-11-17 20:14:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,75 +6,91 @@ from merge_sort import merge
 from os import system, name, _exit
 from time import sleep
 
-clear = lambda: system('cls' if name == 'nt' else 'clear')
-clear()
+if __name__ == '__main__':
 
-lista = []
+  clear = lambda: system('cls' if name == 'nt' else 'clear')
+  clear()
 
-def init():
-  min = 1
-  max = int(input('\nDigite a quantidade de elementos: '))
-  escolha_ordenacao = input('Digite o tipo de ordenação que deseja: ').lower()
-  qtd_vezes = 100
+  lista = []
 
-  for i in range(max):
-    lista.append(valores_aleatorios(min, max))
+  def apresentacao() -> bool:
+    inicializador = input('\nPara iniciar o processo digite "start", para ordenação hibrida digite "hibrido", para ordenação merge digite "merge sort",\npara a ordenação de sort digite "selection sort" e para sair digite "exit": ').lower().strip()
+    if inicializador == 'start':
+      return True
+    elif inicializador == 'exit':
+      return False
+    elif inicializador == 'hibrido':
+      print('\nNão há nada para fazer!'); sleep(2); clear(); apresentacao()
+      return True
+    elif inicializador == 'merge sort':
+      print('\nNão há nada para fazer!'); sleep(2); clear(); apresentacao()
+      return True
+    elif inicializador == 'selection sort':
+      print('\nNão há nada para fazer!'); sleep(2); clear(); apresentacao()
+      return True
+    else:
+      print('\nComando não reconhecido, digite uns dos comando expecificados!'); sleep(2); clear(); apresentacao()
+      return True
 
-  if (escolha_ordenacao == 'selection sort'):
-    selection_sort()
-    tempo_sort = timeit.timeit(stmt=selection_sort, number=qtd_vezes)
-    print(
-      f"Teste repetido {qtd_vezes} vezes\nTempo de duração do teste: {tempo_sort}\nTempo médio do teste {tempo_sort/qtd_vezes}"
-    )
-  elif (escolha_ordenacao == 'merge sort'):
-    merge_sort()
-    tempo_merge = timeit.timeit(stmt=merge_sort, number=qtd_vezes)
-    print(
-      f"Teste repetido {qtd_vezes} vezes\nTempo de duração do teste: {tempo_merge}\nTempo médio do teste {tempo_merge/qtd_vezes}"
-    )
-  elif (escolha_ordenacao == 'hibrido'):
-    hibrida()
-    tempo_hibrido = timeit.timeit(stmt=hibrida, number=qtd_vezes)
-    print(
-      f"Teste repetido {qtd_vezes} vezes\nTempo de duração do teste: {tempo_hibrido}\nTempo médio do teste {tempo_hibrido/qtd_vezes}"
-    )
-  elif (escolha_ordenacao == 'tempo'):
-    comparar_tempo(tempo_sort, tempo_merge, tempo_hibrido, qtd_vezes)
-  elif (escolha_ordenacao == 'exit'):
-    _exit(0)
-  else:
-    print(
-      '\nComando não reconhecido digite uma das alternativas opções "selection sort", "merge sort", "hibrido" "exit"'
-    )
-    sleep(2)
-    init()
+  def valores_aleatorios(min, max):
+    return random.randint(min, max)
 
+  def hibrida():
+    return ordenacao(lista)
 
-  return max
+  def selection_sort():
+    
+    return sort(lista)
 
-def valores_aleatorios(min, max):
-  return random.randint(min, max)
+  def merge_sort():
+    return merge(lista)
 
-def hibrida():
-  return ordenacao(lista)
+  def entradaDado():
+    try:
+      while True:
+        min = 1
+        max = int(input('\nDigite a quantidade de elementos: '))
+        escolha_ordenacao = input('Digite o tipo de ordenação que deseja: ').lower().strip()
+        qtd_vezes = 100
 
-def selection_sort():
-  return sort(lista)
+        for i in range(max):
+          lista.append(valores_aleatorios(min, max))
 
-def merge_sort():
-  return merge(lista)
+        if escolha_ordenacao == None or max == None:
+          print('\nComando não reconhecido, digite uns dos comando expecificados!')
+          sleep(2); clear(); entradaDado()
+        elif escolha_ordenacao == 'exit':
+          _exit(0)
+        elif escolha_ordenacao == 'selection sort':
+          selection_sort()
+          tempo_sort = timeit.timeit(stmt=selection_sort, number=qtd_vezes)
+          print(
+            f"\nTeste repetido {qtd_vezes} vezes\nTempo de duração do teste: {tempo_sort}\nTempo médio do teste {tempo_sort/qtd_vezes}"
+          )
+        elif (escolha_ordenacao == 'merge sort'):
+          merge_sort()
+          tempo_merge = timeit.timeit(stmt=merge_sort, number=qtd_vezes)
+          print(
+            f"\nTeste repetido {qtd_vezes} vezes\nTempo de duração do teste: {tempo_merge}\nTempo médio do teste {tempo_merge/qtd_vezes}"
+          )
+        elif (escolha_ordenacao == 'hibrido'):
+          hibrida()
+          tempo_hibrido = timeit.timeit(stmt=hibrida, number=qtd_vezes)
+          print(
+            f"\nTeste repetido {qtd_vezes} vezes\nTempo de duração do teste: {tempo_hibrido}\nTempo médio do teste {tempo_hibrido/qtd_vezes}"
+          )
 
-def comparar_tempo(sort, merge, hibrido, qtd_vezes):
-  if (sort):
-    print(f'\nTempo de duração do teste sort: {sort}\nTempo médio do teste {sort/qtd_vezes}')
-  elif (merge):
-    print(f'\nTempo de duração do teste merge: {merge}\nTempo médio do teste {merge/qtd_vezes}')
-  elif (hibrido):
-    print(f'\nTempo de duração do teste hibrido: {hibrido}\nTempo médio do teste {hibrido/qtd_vezes}')
+    except Exception as e:
+      if e:
+        print(e)
+      print('\nComando não reconhecido, digite uns dos comando expecificados!')
+      sleep(2); entradaDado()
 
-init()
+  def main():
+    try:
+      if apresentacao():
+        entradaDado()
+    except Exception as e:
+      print(e)
 
-if init() < 128:
-  print("-> Selection Sort <-")
-else:
-  print("-> Merge Sort <-")
+main()  
